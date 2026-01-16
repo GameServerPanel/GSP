baaa9624549930a7d0082d80860cd1df73af6311
Frank Harris <iaretechnician@gmail.com>
2026-01-16 01:46:33 -0600

    MAJOR updated for steam workshop

--- modules/steam_workshop/module.php
+++ modules/steam_workshop/module.php
@@ -23,7 +23,7 @@
  */
 // Module general information
 $module_title = "Steam Workshop";
-$module_version = "1.1";
+$module_version = "2.0";
 $db_version = 0;
 $module_required = TRUE;
 $module_menus = array(array( 'subpage' => 'workshop_admin', 'name'=>'Steam Workshop', 'group'=>'admin' ));
